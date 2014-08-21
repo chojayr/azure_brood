bd80f9cef7226692fa1e3ae84d203b1ec22fb7e6
Luis Lavina <llavina@gorocketlabs.com>
2014-08-22 01:05:37 +0800

    add notes and comment on deploy

--- deploy_azurevm.rb
+++ deploy_azurevm.rb
@@ -31,17 +31,17 @@ ymlhost.each do |vm|
   #This is to create VM and to start deployment
   params = {
     :vm_name => "#{vm}",
-    :vm_user => 'mapua',
-    :image => 'a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-201407.01-en.us-127GB.vhd',
-    :password => 'mapuatest2014!',
+    :vm_user => 'yourusername',
+    :image => '0b11de9248dd4d87b18621318e037d37__RightImage-Ubuntu-12.04-x64-v13.4', #sample available image
+    :password => 'yourpassword',
     :location => 'Southeast Asia'
   }
 
   option = { 
     :cloud_service_name => "#{vm}",
     :deployment_name => "#{vm}",
-    :tcp_endpoints => '80,8080,8443:443', #ports you need to open
-    :certificate_file => '/home/llavina/.ssh/pem/llavina.pem',
+    :tcp_endpoints => '22,80,8080,8443:443', #ports you need to open
+    :certificate_file => '/path/to/your/file.pem',
     :vm_size => 'Basic_A1'
   }
 
